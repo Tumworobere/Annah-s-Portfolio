c6d034eafbd1fc845dae0632cc7a38809c111373
tumworobere <annaht.annah@gmail.com>
2025-04-21 14:40:45 +0300

    Update the About Page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -20,14 +20,14 @@ const About = () => {
             <article className="about_card">
               <FaAward className="about_icon" />
               <h5>Experience</h5>
-              <small>3+ years</small>
+              <small>4+years</small>
             </article>
             <article className="about_card">
               <FaCertificate className="about_icon" />
               <h5>Certificates</h5>
               <small>10+</small>
             </article>
-            <article className="about_card">
+            <article className="about_card"> 
               <FaFolder className="about_icon" />
               <h5>Projects</h5>
               <small>40+ projects completed</small>
